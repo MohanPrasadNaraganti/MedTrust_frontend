c74d76cfdea369bdefcc1c1344629f0aba4dae14
MohanPrasadNaraganti <naraga65@students.rowan.edu>
2024-02-27 12:57:19 -0500

    hambarger

--- src/headers/Header.js
+++ src/headers/Header.js
@@ -11,8 +11,7 @@ import { loginUser } from '../actions/loginAction';
 import cartReducer from '../reducers/cartReducer';
 import axios from 'axios';
 import API_URL from '../Util';
-
-
+import './Hamburger.css'; // Import your CSS file for styling
 
 const Header = () => {
 
@@ -24,7 +23,13 @@ const Header = () => {
     const [click, setClick] = useState(false);
     const [len, setLen] = useState(cart.length)
     const dispatch = useDispatch();
-  
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
 
     const handleClick = () => setClick(!click);
     useEffect(() => {
@@ -41,12 +46,12 @@ const Header = () => {
         const firstLogin = localStorage.getItem('myData')
         if (firstLogin) {
             setIsLogin(true)
-          
-        }
-       
-    }, [role,len,cart])
 
-   
+        }
+
+    }, [role, len, cart])
+
+
     const Logout = () => {
         // console.log(userDetails?.user?._id,cartItems,"logoff")
         const updateUserCart = async () => {
@@ -69,34 +74,54 @@ const Header = () => {
             <nav className="navbar sticky-top ">
                 <div className="nav-container">
 
-                    <h2><Link to="/"><img src={LogoImg} width="80px" height="80px" alt="companyLogo"/> Med Trust || Online Pharmacy </Link></h2>
-                    <h2>  <NavLink exact to="/" className="nav-logo">
-                        Med Trust <i style={{ color: "lightgreen" }} className="fa fa-check" aria-hidden="true"></i></NavLink> </h2>
+                    <h2><Link to="/"><img src={LogoImg} width="80px" height="80px" alt="companyLogo" /> Med Trust || Online Pharmacy </Link></h2>
+                    {/* <h2>  <NavLink exact to="/" className="nav-logo">
+                        Med Trust <i style={{ color: "lightgreen" }} className="fa fa-check" aria-hidden="true"></i></NavLink> </h2> */}
 
                     <div className="headerbody">
+                        <div style={{display:"flex",justifyContent:"flex-end"}}> 
+                        <div className="hamburger-menu">
+                            <div className={`hamburger ${!isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                                <div className="line"></div>
+                                <div className="line"></div>
+                                <div className="line"></div>
+                            </div>
+                            {
+                                !isOpen && 
+                                (<>
+                                    {role === 1 ?
+                                        (<ul className={click ? "nav-menu active" : "nav-menu"}>
+                                            <li className="nav-item"><NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick} > Home </NavLink></li>
+                                            <li className="nav-item"><NavLink exact to="/product" activeClassName="active" className="nav-links" onClick={handleClick} > Products </NavLink></li>
+                                            <li className="nav-item"><NavLink exact to="/createproduct" activeClassName="active" className="nav-links" onClick={handleClick} > CreateProduct </NavLink></li>
+                                            <li className="nav-item"><NavLink exact to="/createcategory" activeClassName="active" className="nav-links" onClick={handleClick} > CreateCategory </NavLink></li>
+                                            <li className="nav-item"><NavLink exact to="/orderhistory" activeClassName="active" className="nav-links" onClick={handleClick} > OrdersHistory </NavLink></li>
+                                            <li className="nav-item"> {islogin ? <a style={{ cursor: "pointer", color: "#0d6efd" }} onClick={Logout}>Logout </a> : <NavLink exact to="/Login" activeClassName="active" className="nav-links" onClick={handleClick} > Login </NavLink>}</li>
 
-                        {role === 1 ?
-                            <ul className={click ? "nav-menu active" : "nav-menu"}>
-                                <li className="nav-item"><NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick} > Home </NavLink></li>
-                                <li className="nav-item"><NavLink exact to="/product" activeClassName="active" className="nav-links" onClick={handleClick} > Products </NavLink></li>
-                                <li className="nav-item"><NavLink exact to="/createproduct" activeClassName="active" className="nav-links" onClick={handleClick} > CreateProduct </NavLink></li>
-                                <li className="nav-item"><NavLink exact to="/createcategory" activeClassName="active" className="nav-links" onClick={handleClick} > CreateCategory </NavLink></li>
-                                <li className="nav-item"><NavLink exact to="/orderhistory" activeClassName="active" className="nav-links" onClick={handleClick} > OrdersHistory </NavLink></li>
-                                <li className="nav-item"> {islogin ? <a style={{ cursor: "pointer", color: "#0d6efd" }} onClick={Logout}>Logout </a> : <NavLink exact to="/Login" activeClassName="active" className="nav-links" onClick={handleClick} > Login </NavLink>}</li>
+                                        </ul>) :
+                                    ( 
+                                    <><ul className={click ? "nav-menu active" : "nav-menu"}>
+                                        <li className="nav-item"> {islogin ? <Link to="/userpage">Home </Link> : <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick} > Home </NavLink>}</li>
+                                        <li className="nav-item"> <NavLink exact to="/product" activeClassName="active" className="nav-links" onClick={handleClick} > Product </NavLink></li>
+                                        <li className="nav-item"> <NavLink exact to="/about" activeClassName="active" className="nav-links" onClick={handleClick} > About </NavLink></li>
+                                        <li className="nav-item"> {islogin ? <Link to="/Cart"><i className="fas fa-cart-plus"></i> Cart ({len})</Link> : <NavLink exact to="/register" activeClassName="active" className="nav-links" onClick={handleClick} > Register </NavLink>}</li>
+                                        <li className="nav-item"> {islogin ? <a style={{ cursor: "pointer", color: "#0d6efd" }} onClick={Logout}>Logout </a> : <NavLink exact to="/Login" activeClassName="active" className="nav-links" onClick={handleClick} > Login </NavLink>}</li>
 
-                            </ul> :
-                            <ul className={click ? "nav-menu active" : "nav-menu"}>
-                                <li className="nav-item"> {islogin ? <Link to="/userpage">Home </Link> : <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick} > Home </NavLink>}</li>
-                                <li className="nav-item"> <NavLink exact to="/product" activeClassName="active" className="nav-links" onClick={handleClick} > Product </NavLink></li>
-                                <li className="nav-item"> <NavLink exact to="/about" activeClassName="active" className="nav-links" onClick={handleClick} > About </NavLink></li>
-                                <li className="nav-item"> {islogin ? <Link to="/Cart"><i className="fas fa-cart-plus"></i> Cart ({len})</Link> : <NavLink exact to="/register" activeClassName="active" className="nav-links" onClick={handleClick} > Register </NavLink>}</li>
-                                <li className="nav-item"> {islogin ? <a style={{ cursor: "pointer", color: "#0d6efd" }} onClick={Logout}>Logout </a> : <NavLink exact to="/Login" activeClassName="active" className="nav-links" onClick={handleClick} > Login </NavLink>}</li>
+                                    </ul>
+                                    <div className="nav-icon" onClick={handleClick}>
+                                        <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+                                    </div>
+                                    </>)
+                                    
 
-                            </ul>
-                        }
-                        <div className="nav-icon" onClick={handleClick}>
-                            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+
+                                     } </>)
+                            }
+
+
                         </div>
+                        </div>
+                      
                     </div>
                 </div>
             </nav>
